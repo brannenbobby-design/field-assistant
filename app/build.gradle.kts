b3420b0993c982314c302e21fdc16665a98c88c5
brannenbobby-design <brannenbobby@gmail.com>
2026-09-09 18:14:01 -0500

    Bump Photo Rescue hardened deep scan build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.brannenservices.fieldassistant"
         minSdk = 26
         targetSdk = 35
-        versionCode = 7
-        versionName = "0.4.0-photo-rescue"
+        versionCode = 8
+        versionName = "0.5.0-photo-rescue"
     }
 
     buildTypes {
