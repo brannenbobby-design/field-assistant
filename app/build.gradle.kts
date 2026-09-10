7a540107edf5a2c3ee2f7e12870ff28e6599d735
brannenbobby-design <brannenbobby@gmail.com>
2026-09-10 14:17:49 -0500

    Set Photo Rescue version to 0.10

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.brannenservices.fieldassistant"
         minSdk = 26
         targetSdk = 35
-        versionCode = 9
-        versionName = "0.9.0-photo-rescue"
+        versionCode = 10
+        versionName = "0.10.0-photo-rescue"
     }
 
     buildTypes {
