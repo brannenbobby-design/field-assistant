0ddb1e23f48ce7594b35cbb82192ea85ea883ec1
brannenbobby-design <brannenbobby@gmail.com>
2026-09-10 14:46:26 -0500

    Reset app version for Florida Man prototype

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.brannenservices.fieldassistant"
         minSdk = 26
         targetSdk = 35
-        versionCode = 11
-        versionName = "0.11.0-photo-rescue"
+        versionCode = 1
+        versionName = "0.1.0-florida-man"
     }
 
     buildTypes {
@@ -31,6 +31,4 @@ android {
 dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.documentfile:documentfile:1.0.1")
 }
