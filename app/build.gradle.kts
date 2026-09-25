cb493a06c0152e5c2681005f6914911f26ab9596
brannenbobby-design <brannenbobby@gmail.com>
2026-09-25 11:47:59 -0500

    Add adaptive portrait display for AA Mirror Probe v0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.brannen.aamirrorprobe"
         minSdk = 28
         targetSdk = 35
-        versionCode = 2
-        versionName = "0.2"
+        versionCode = 3
+        versionName = "0.3"
     }
 }
 dependencies {
