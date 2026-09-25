349fa4cd77ed6d5cd6977c0cd9d6424333dfc794
brannenbobby-design <brannenbobby@gmail.com>
2026-09-25 12:28:23 -0500

    Restore Android Auto launcher compatibility in v0.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.brannen.aamirrorprobe"
         minSdk = 28
         targetSdk = 35
-        versionCode = 3
-        versionName = "0.3"
+        versionCode = 4
+        versionName = "0.4"
     }
 }
 dependencies {
