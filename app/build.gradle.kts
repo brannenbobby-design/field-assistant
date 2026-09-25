c90fde069a8e1211df1ce6e7052e8ba3429733cf
brannenbobby-design <brannenbobby@gmail.com>
2026-09-25 07:34:36 -0500

    Align Java and Kotlin JVM targets

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,6 +5,10 @@ plugins {
 android {
     namespace = "com.brannen.aamirrorprobe"
     compileSdk = 35
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
     defaultConfig {
         applicationId = "com.brannen.aamirrorprobe"
         minSdk = 28
