95766af537e0480ffd1324813d45f45a9e763867
brannenbobby-design <brannenbobby@gmail.com>
2026-09-25 06:42:21 -0500

    Repurpose Field Assistant as AA Mirror Probe

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,32 +2,19 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
 }
-
 android {
     namespace = "com.brannenservices.fieldassistant"
     compileSdk = 35
-
     defaultConfig {
         applicationId = "com.brannenservices.fieldassistant"
-        minSdk = 26
+        minSdk = 28
         targetSdk = 35
-        versionCode = 116
-        versionName = "0.1.0-build116"
+        versionCode = 200
+        versionName = "aa-mirror-probe-0.1"
     }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
+    compileOptions { sourceCompatibility = JavaVersion.VERSION_17; targetCompatibility = JavaVersion.VERSION_17 }
     kotlinOptions { jvmTarget = "17" }
 }
-
 dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
